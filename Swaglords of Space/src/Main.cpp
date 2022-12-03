32916d016d0e40ae56405c1fd83716cfc6b9cca1
Adham <adhamnotheare@gmail.com>
2022-12-03 15:38:56 +0530

    Nothing

--- Swaglords of Space/src/Main.cpp
+++ Swaglords of Space/src/Main.cpp
@@ -16,11 +16,12 @@ int main()
 
 	SWAG_TRACE("SwagLord v{}.{}", 2, 20);
 
-	Swag::Game game(SCREEN_HEIGHT, SCREEN_WIDTH, "SWAGLORDS OF SPACE", DEV_SCREEN, DEV_SCREEN + 50, "DEVELOPER WINDOW", false);
+	Swag::Game* game = new Swag::Game(SCREEN_HEIGHT, SCREEN_WIDTH, "SWAGLORDS OF SPACE", DEV_SCREEN, DEV_SCREEN + 50, "DEVELOPER WINDOW", false);
 
 	SWAG_INFO("Game Destroyed");
 
 	Swag::LogManager::ShutDown();
+	free(game);
 
 	std::cin.get();
 
